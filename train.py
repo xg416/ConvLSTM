dc06b7fb0610466a69b95cbfbfdc015626b58d8b
XG Zhang <1065965899@qq.com>
2019-04-22 17:55:22 -0400

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import numpy as np
 from torch.autograd import Variable
 from Res_clstm_MN import Res_clstm_MN
 import time
-from datagen import isoTrainImageGenerator, isoTestImageGenerator
+from datagen import isoImageGenerator
 import inputs as data
 
 def poly_lr_scheduler(optimizer, init_lr, iter, lr_decay_iter=1,
@@ -30,7 +30,9 @@ def poly_lr_scheduler(optimizer, init_lr, iter, lr_decay_iter=1,
     lr = init_lr*(1 - iter/max_iter)**power
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
-        
+
+    return lr
+
 def success(output, target):
     success_number = 0
     predict = output.argmax(dim = 1)
@@ -43,10 +45,10 @@ def success(output, target):
 
 def train(training_datalist, batch_size, seq_len, num_classes, cfg_modality, device, max_iter = 10, current = 0):
     AvgLoss = 0
-    batch_num = len(training_datalist) / batch_size
-    accuracy = 0
-    for data, label in isoTestImageGenerator(training_datalist, batch_size, seq_len, num_classes, cfg_modality):
-        poly_lr_scheduler(optimizer, init_lr = 0.0001, iter = current, lr_decay_iter=1,max_iter=10, power=1.5)
+    batch_num = len(training_datalist[0]) / batch_size
+    success_number = 0
+    for data, label in isoImageGenerator(training_datalist, batch_size, seq_len, num_classes, cfg_modality, Training = True):
+        lr = poly_lr_scheduler(optimizer, init_lr = 0.0001, iter = current, lr_decay_iter=1,max_iter=10, power=1.5)
         data = data.transpose(0,4,1,2,3)
         input = torch.from_numpy(data).float().to(device)
         target = torch.from_numpy(label).argmax(dim = 1).to(device)
@@ -55,15 +57,17 @@ def train(training_datalist, batch_size, seq_len, num_classes, cfg_modality, dev
         loss = loss_fn(output, target)
         loss.backward()
         optimizer.step()
-        success_number += success(output, target)
+        s =success(output, target)
+        success_number += s
         AvgLoss += float(loss)
+        print(s, float(loss))
     return AvgLoss / batch_num, success_number / batch_num
 
 def validate(valid_datalist, batch_size, seq_len, num_classes, cfg_modality, device):
     AvgLoss = 0
-    batch_num = len(valid_datalist) / batch_size
-    accuracy = 0
-    for data, label in isoTrainImageGenerator(valid_datalist, batch_size, seq_len, num_classes, cfg_modality):
+    batch_num = len(valid_datalist[0]) / batch_size
+    success_number = 0
+    for data, label in isoImageGenerator(valid_datalist, batch_size, seq_len, num_classes, cfg_modality, Training = False):
         data = data.transpose(0,4,1,2,3)
         input = torch.from_numpy(data).float().to(device)
         target = torch.from_numpy(label).argmax(dim = 1).to(device)
@@ -77,33 +81,42 @@ def validate(valid_datalist, batch_size, seq_len, num_classes, cfg_modality, dev
 
 if __name__ == '__main__':
 
-    training_datalist = '/home/isat-deep/Documents/isoGD/train_rgb_list.txt'
-    _,train_labels = data.load_iso_video_list(training_datalist)
-
+    rgb_train_datalist = '/home/isat-deep/Documents/isoGD/train_rgb_list.txt'
+    depth_train_datalist = '/home/isat-deep/Documents/isoGD/train_depth_list.txt'
+    flow_train_datalist = '/home/isat-deep/Documents/isoGD/train_flow_list.txt'
+    rgb_valid_datalist = '/home/isat-deep/Documents/isoGD/valid_rgb_list.txt'
+    depth_valid_datalist = '/home/isat-deep/Documents/isoGD/valid_depth_list.txt'
+    flow_valid_datalist = '/home/isat-deep/Documents/isoGD/valid_flow_list.txt'
+    training_datalist = []
+    valid_datalist = []
     model_path = '/home/isat-deep/Documents/isoGD/model.pkl'
     # Modality
     RGB = 0
-    Depth = 1 
+    Depth = 1
     Flow = 2
-    cfg_modality = RGB
-    if cfg_modality==RGB:
-        str_modality = 'rgb'
-        input_channels = 3
-    elif cfg_modality==Depth:
-        str_modality = 'depth'
-        input_channels = 1
-    elif cfg_modality==Flow:
-        str_modality = 'flow'
-        input_channels = 1
+    input_channels = 0
+    cfg_modality = [RGB, Depth]
+    if 0 in cfg_modality:
+        input_channels += 3
+        training_datalist.append(rgb_train_datalist)
+        valid_datalist.append(rgb_valid_datalist)
+    if 1 in cfg_modality:
+        input_channels += 3
+        training_datalist.append(depth_train_datalist)
+        valid_datalist.append(depth_valid_datalist)
+    if 2 in cfg_modality:
+        input_channels += 3
+        training_datalist.append(flow_train_datalist)
+        valid_datalist.append(flow_valid_datalist)
 
-    batch_size = 2
+    batch_size = 8
     seq_len = 32
     num_classes = 249
     total_epoch = 20
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     Model = Res_clstm_MN(input_shape = (batch_size, input_channels, seq_len, 112, 112), \
-        number_class = num_classes, AttenMethod = 'd').to(device)
+        number_class = num_classes, AttenMethod = 'a').to(device)
 
     loss_fn = nn.CrossEntropyLoss()
 
